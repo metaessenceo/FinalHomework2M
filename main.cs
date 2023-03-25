fedf4e84402fa408544e5ef5c61d8dacb78f7b6e
Dmitry Gulakov <essencegodof@gmail.com>
2023-03-25 18:41:28 +0300

    writing + if length <= 3

--- main.cs
+++ main.cs
@@ -2,8 +2,11 @@ string[] inputArray = {"hello", "2", "world", ":-)"};
 
 for (int i = 0; i < inputArray.Length; i++) {
     string element = inputArray[i];
-    
+
     Console.Write(element.Length);
+    if (element.Length <=3) {
+        Console.Write("+");
+    }
     Console.Write(" ");
     Console.WriteLine(element);
 }
