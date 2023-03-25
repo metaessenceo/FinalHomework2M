cdb116ff463f290c674a0e0194b01b0b1e94ea21
Dmitry Gulakov <essencegodof@gmail.com>
2023-03-25 18:39:17 +0300

    writing array with strings length

--- main.cs
+++ main.cs
@@ -2,5 +2,8 @@ string[] inputArray = {"hello", "2", "world", ":-)"};
 
 for (int i = 0; i < inputArray.Length; i++) {
     string element = inputArray[i];
+    
+    Console.Write(element.Length);
+    Console.Write(" ");
     Console.WriteLine(element);
 }
