f46f6ea13698faae8942ab4fa4d7aef8204e49eb
Dmitry Gulakov <essencegodof@gmail.com>
2023-03-25 18:48:53 +0300

    added output array and fill it elements that <=3

--- main.cs
+++ main.cs
@@ -1,10 +1,12 @@
 string[] inputArray = {"hello", "2", "world", ":-)"};
+string[] outputArray = new string[inputArray.Length];
 
 for (int i = 0; i < inputArray.Length; i++) {
     string element = inputArray[i];
 
     if (element.Length <=3) {
         Console.WriteLine(element);
-    }
 
+        outputArray[i] = element;
+    }
 }
