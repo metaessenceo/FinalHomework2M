56992c6b869e059f12e247625704281eb2936a48
Dmitry Gulakov <essencegodof@gmail.com>
2023-03-25 18:43:47 +0300

    now writing only strings that length <= 3

--- main.cs
+++ main.cs
@@ -3,10 +3,8 @@ string[] inputArray = {"hello", "2", "world", ":-)"};
 for (int i = 0; i < inputArray.Length; i++) {
     string element = inputArray[i];
 
-    Console.Write(element.Length);
     if (element.Length <=3) {
-        Console.Write("+");
+        Console.WriteLine(element);
     }
-    Console.Write(" ");
-    Console.WriteLine(element);
+
 }
